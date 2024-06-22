8c84f37c4f9a85c93acdbb253eaad53ed46cbf15
Shridhant <shridhantjaiswal@gmail.com>
2024-06-22 13:25:59 +0530

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+// const sections = document.querySelectorAll("section");
+
+// window.addEventListener("scroll", smooth);
+
+// smooth();
+
+// function smooth() {
+//   const triggerBottom = (window.innerHeight / 5) * 4;
+
+//   boxes.forEach((section) => {
+//     const top = section.getBoundingClientRect().top;
+//     if (top < triggerBottom) {
+//       section.classList.add("show");
+//     } else {
+//       section.classList.remove("show");
+//     }
+//   });
+// }
